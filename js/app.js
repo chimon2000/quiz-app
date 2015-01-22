846c00f3cbf45f604fb5e31faae406d928c0e12c
cecyc <cecycorrea@gmail.com>
2015-01-21 22:28:07 -0600

    Uploading HTML + CSS files

---
+++ js/app.js
@@ -0,0 +1,18 @@
+var quizQuestions = [{
+  question: "Where do Chows come from?",
+  choices: ["Japan", "Indonesia", "Mexico", "China"],
+  answer: 4,
+  ,{
+  question: "What does Chow Chow mean in Chinese?",
+  choices: ["Puffy lion dog", "Panda", "Elephant", "Horse"],
+  answer: 1,
+  ,{
+  question: "Chows descend from?",
+  choices: ["Lions", "Wolves", "Polar bears", "Grizzly bears"],
+  answer: 2,
+  ,{
+  question: "How often should you brush your Chow?",
+  choices: ["Twice a week", "Once a month", "Once every six months", "Never"],
+  answer: 1,
+];
+
